43ab2351a88d84387ed24b311921a90afbd1ed47
wowimnick <factionsinsanity@gmail.com>
2021-05-22 03:46:00 -0400

    Added more stuff
    Added fruit and the snake now grows and has separate body parts.

--- snake.py
+++ snake.py
@@ -1,23 +1,36 @@
+import random
+
 import pygame
 from pygame.locals import *
 import sys
 import time
 
+SIZE = 40
+
 
 class Snake:
-    def __init__(self, parent_screen):
+    LENGTH = 5
+
+    def __init__(self, parent_screen, length):
+        self.food = pygame.image.load("food.png").convert()
+        self.food = pygame.transform.scale(self.food, (20, 20))
+        self.length = 5
+        self.score = 0
         self.parent_screen = parent_screen
-        self.block_x = 100
-        self.block_y = 100
+        self.block_x = [SIZE] * length
+        self.block_y = [SIZE] * length
 
         self.block = pygame.image.load("head.png").convert()
         self.block = pygame.transform.scale(self.block, (20, 20))
         self.direction = 'right'
+        self.y_food = random.randint(0, 920)
+        self.x_food = random.randint(0, 920)
 
     def draw(self):
         self.parent_screen.fill((2, 0, 50))
-        self.parent_screen.blit(self.block, (self.block_x, self.block_y))
-        self.parent_screen.blit(self.block, (self.block_x, self.block_y))
+        for i in range(self.length):
+            self.parent_screen.blit(self.block, (self.block_x[i], self.block_y[i]))
+            self.parent_screen.blit(self.food, (self.x_food, self.y_food))
 
     def move_left(self):
         self.draw()
@@ -39,19 +52,43 @@ class Snake:
         if self.direction != "up":
             self.direction = 'down'
 
+    def inc_length(self):
+        self.length += 1
+        self.block_x.append(-1)
+        self.block_y.append(-1)
+
     def walk(self):
+        for i in range(self.length - 1, 0, -1):
+            self.block_x[i] = self.block_x[i - 1]
+            self.block_y[i] = self.block_y[i - 1]
+            print(self.x_food, self.y_food)
+            if self.block_x[0] + 20 > self.x_food > self.block_x[0] - 20 and self.block_y[
+                0] + 20 > self.y_food > self.block_y[0] - 20:
+                self.score += 1
+                self.inc_length()
+
+                self.x_food = random.randint(0, 920)
+                self.y_food = random.randint(0, 920)
+
+
+
+                self.draw()
+                print(self.score)
+                print(self.length)
+
         if self.direction == "left":
-            self.block_x -= 20
+            self.block_x[0] -= 20
             self.draw()
         if self.direction == "right":
-            self.block_x += 20
+            self.block_x[0] += 20
             self.draw()
         if self.direction == "up":
-            self.block_y -= 20
+            self.block_y[0] -= 20
             self.draw()
         if self.direction == "down":
-            self.block_y += 20
+            self.block_y[0] += 20
             self.draw()
+
 
 class Game:
     def __init__(self):
@@ -63,8 +100,7 @@ class Game:
         pygame.init()
         self.surface = pygame.display.set_mode((self.WINDOW_HEIGHT, self.WINDOW_WIDTH))
         self.surface.fill((2, 0, 50))
-        self.snake = Snake(self.surface)
-        self.snake.draw()
+        self.snake = Snake(self.surface, 5)
 
     def run(self):
         running = True
@@ -72,6 +108,7 @@ class Game:
         while running:
             game.drawGrid()
             pygame.display.flip()
+
             for event in pygame.event.get():
                 if event.type == KEYDOWN:
                     if event.key == K_w:
@@ -90,8 +127,9 @@ class Game:
                     pygame.quit()
                     sys.exit(0)
             pygame.time.wait(10)
+            self.snake.draw()
             self.snake.walk()
-            time.sleep(0.2)
+            time.sleep(0.15)
 
     def drawGrid(self):
         blockSize = 20  # Set the size of the grid block
